1aff335828c3a74947cd495927621f5be10792b9
Mahmoud Ahmed <mhmoud.aboalazm@gmail.com>
2021-12-06 19:11:27 +0200

    Add the data access class that loads messages and replies and saves data using Dapper and SqlClient

---
+++ DataAccess/DbAccess/ISqlDataAccess.cs
@@ -0,0 +1,13 @@
+﻿using DataAccess.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DataAccess.DbAccess
+{
+    public interface ISqlDataAccess
+    {
+        Task<IEnumerable<MessageModel>> LoadMessages<U>(string storedProcedure, U parameters, string connectionId = "Default");
+        Task<IEnumerable<ReplyModel>> LoadReplies<U>(string storedProcedure, U parameters, string connectionId = "Default");
+        Task SaveData<T>(string storedProcedure, T parameters, string connectionId = "Default");
+    }
+}
---
+++ DataAccess/DbAccess/SqlDataAccess.cs
@@ -0,0 +1,68 @@
+﻿using Dapper;
+using System.Data;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.Collections;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using DataAccess.Models;
+
+namespace DataAccess.DbAccess
+{
+    public class SqlDataAccess : ISqlDataAccess
+    {
+        private readonly IConfiguration _config;
+
+        public SqlDataAccess(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public async Task<IEnumerable<MessageModel>> LoadMessages<U>(
+            string storedProcedure,
+            U parameters,
+            string connectionId = "Default")
+        {
+            using IDbConnection connection = new SqlConnection(_config.GetConnectionString(connectionId));
+
+            return await connection.QueryAsync<MessageModel, UserModel, MessageModel>(
+                storedProcedure,
+                (msg, usr) =>
+                {
+                    msg.User = usr;
+                    return msg;
+                },
+                parameters,
+                commandType: CommandType.StoredProcedure);
+        }
+
+        public async Task<IEnumerable<ReplyModel>> LoadReplies<U>(
+            string storedProcedure,
+            U parameters,
+            string connectionId = "Default")
+        {
+            using IDbConnection connection = new SqlConnection(_config.GetConnectionString(connectionId));
+
+            return await connection.QueryAsync<ReplyModel, UserModel, ReplyModel>(
+                storedProcedure,
+                (rply, usr) =>
+                {
+                    rply.User = usr;
+                    return rply;
+                },
+                parameters,
+                commandType: CommandType.StoredProcedure);
+        }
+
+        public async Task SaveData<T>(
+            string storedProcedure,
+            T parameters,
+            string connectionId = "Default")
+        {
+            using IDbConnection connection = new SqlConnection(_config.GetConnectionString(connectionId));
+
+            await connection.ExecuteAsync(storedProcedure, parameters,
+                commandType: CommandType.StoredProcedure);
+        }
+    }
+}
