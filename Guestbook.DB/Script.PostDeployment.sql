78f5fabb0eaccba6397e56e1c5b0a765d5831788
Mahmoud Ahmed <mhmoud.aboalazm@gmail.com>
2021-12-06 16:30:20 +0200

    Fixing some issue with the post deployment script
    Replace double quotation mark with single ones

--- Guestbook.DB/Script.PostDeployment.sql
+++ Guestbook.DB/Script.PostDeployment.sql
@@ -1,25 +1,25 @@
 ﻿if not exists (select 1 from dbo.[User])
 begin
 	insert into dbo.[User] (FirstName, LastName, Email)
-	values ("Mahmoud", "Ahmed", "foo@bar.com"),
-	("Amr", "Ahmed", "bez@bar.com"),
-	("Dalia", "Ahmed", "dha@bar.com")
+	values ('Mahmoud', 'Ahmed', 'foo@bar.com'),
+	('Amr', 'Ahmed', 'bez@bar.com'),
+	('Dalia', 'Ahmed', 'dha@bar.com')
 end
 
 if not exists (select 1 from dbo.[Message])
 begin
 	insert into dbo.[Message] (Content, UserId)
-	values ("This a dummy text", 1),
-	("This a test", 1),
-	("Hello World", 2),
-	("Testing", 3)
+	values ('This a dummy text', 1),
+	('This a test', 1),
+	('Hello World', 2),
+	('Testing', 3)
 end
 
-if not exists (select 1 from dbo.[Message])
+if not exists (select 1 from dbo.[Reply])
 begin
 	insert into dbo.[Reply] (Content, MessageId, UserId)
-	values ("Good morning today", 1, 2),
-	("test test test test", 1, 3),
-	("This is a reply", 2, 1),
-	("Testing again", 3, 2)
+	values ('Good morning today', 1, 2),
+	('test test test test', 1, 3),
+	('This is a reply', 2, 1),
+	('Testing again', 3, 2)
 end
