902a7e46f46fe16e22131a114e6ca71abe7e97be
Mahmoud Ahmed <mhmoud.aboalazm@gmail.com>
2021-12-06 15:13:32 +0200

    Add stored procedures for accessing the DB
    Adds a stored procedure for getting all the messages data with their user's info

---
+++ Guestbook.DB/dbo/storedProcedures/spMessage_GetAll.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[spMessage_GetAll]
+AS
+begin
+	select msg.Id, msg.Content, msg.CreatedAt, usr.FirstName, usr.LastName, usr.Email
+	from dbo.[Message] as msg inner join dbo.[User] as usr
+	on (msg.UserId = usr.Id)
+	order by msg.CreatedAt desc;
+end
