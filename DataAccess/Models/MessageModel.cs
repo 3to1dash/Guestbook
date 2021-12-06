e7a93fd5753e0c24f3c286ba5fbedc2454394478
Mahmoud Ahmed <mhmoud.aboalazm@gmail.com>
2021-12-06 17:29:43 +0200

    Add the User, Message and Reply models

---
+++ DataAccess/Models/MessageModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Models
+{
+    public class MessageModel
+    {
+        public int Id { get; set; }
+        public String Content { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public UserModel User { get; set; }
+    }
+}
---
+++ DataAccess/Models/UserModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String Email { get; set; }
+    }
+}
