87eae1cf8fefa39430958e0f3ec51ddeaac3e89d
Mahmoud Ahmed <mhmoud.aboalazm@gmail.com>
2021-12-06 14:12:18 +0200

    Add the Message table

---
+++ Guestbook.DB/dbo/Tables/Message.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Message]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Content] NVARCHAR(50) NOT NULL, 
+    [UserId] INT NOT NULL, 
+    [CreatedAt] DATETIME NOT NULL, 
+    CONSTRAINT [FK_Message_User] FOREIGN KEY ([UserId]) REFERENCES [User]([Id]) ON DELETE CASCADE,
+)
+
+GO
+
+CREATE INDEX [IX_Message_CreatedAt] ON [dbo].[Message] ([CreatedAt])
