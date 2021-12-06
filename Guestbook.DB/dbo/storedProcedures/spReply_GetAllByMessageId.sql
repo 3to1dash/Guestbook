e93aaad6c0c58fccbcc4416046eaec98294919e1
Mahmoud Ahmed <mhmoud.aboalazm@gmail.com>
2021-12-06 15:47:31 +0200

    Add a stored procedure for getting a specific message's replies data by message id with its user's info

---
+++ Guestbook.DB/dbo/storedProcedures/spReply_GetAllByMessageId.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[spReply_GetAllByMessageId]
+	@MessageId int
+AS
+begin
+	select rply.Content, rply.CreatedAt, usr.FirstName, usr.LastName, usr.Email
+	from (select *
+		  from dbo.[Reply]
+		  where MessageId = @MessageId) as rply
+	inner join dbo.[User] as usr
+	on rply.UserId = usr.Id
+	order by rply.CreatedAt desc;
+end
