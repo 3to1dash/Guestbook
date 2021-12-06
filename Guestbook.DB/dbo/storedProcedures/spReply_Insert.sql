bbfe8c331c184f24a3f6c9916237a3ade41712b6
Mahmoud Ahmed <mhmoud.aboalazm@gmail.com>
2021-12-06 16:19:43 +0200

    Add sp for inserting replies to the DB

---
+++ Guestbook.DB/dbo/storedProcedures/spReply_Insert.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[spReply_Insert]
+	@Content nvarchar(50),
+	@MessageId int,
+	@UserId int
+AS
+begin
+	insert into dbo.[Reply] (Content, MessageId, UserId)
+	values (@Content, @MessageId, @UserId);
+end
