3d1f5bd19c61c644a4e18d281d2f3d3de4442c9b
Mahmoud Ahmed <mhmoud.aboalazm@gmail.com>
2021-12-06 15:35:26 +0200

    Add a stored procedure for getting a specific message data by its id with its user's info

---
+++ Guestbook.DB/dbo/storedProcedures/spMessage_Get.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[spMessage_Get]
+	@Id int
+AS
+begin
+	select msg.Id, msg.Content, msg.CreatedAt, usr.FirstName, usr.LastName, usr.Email
+	from dbo.[Message] as msg
+	inner join dbo.[User] as usr
+	on (msg.UserId = usr.Id)
+	where msg.Id = @Id;
+end
