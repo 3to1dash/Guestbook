adf8d431608e6029b9e5413100aea02e5e3f3d98
Mahmoud Ahmed <mhmoud.aboalazm@gmail.com>
2021-12-06 16:19:01 +0200

    Add stored procedures to insert and update the message table

---
+++ Guestbook.DB/dbo/storedProcedures/spMessage_Insert.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[spMessage_Insert]
+	@Content nvarchar(50),
+	@UserId int
+AS
+begin
+	insert into dbo.[Message] (Content, UserId)
+	values (@Content, @UserId);
+end
---
+++ Guestbook.DB/dbo/storedProcedures/spMessage_Update.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[spMessage_Update]
+	@Id int,
+	@Content nvarchar(50)
+AS
+begin
+	update dbo.[Message]
+	set Content = @Content
+	where Id = @Id;
+end
