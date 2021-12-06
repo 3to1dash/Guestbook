d75d36adbb56649aa920806a5d715279e1336ea4
Mahmoud Ahmed <mhmoud.aboalazm@gmail.com>
2021-12-06 14:21:30 +0200

    Add the Reply table

---
+++ Guestbook.DB/dbo/Tables/Reply.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[Reply]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Content] NVARCHAR(50) NOT NULL, 
+    [MessageId] INT NOT NULL, 
+    [UserId] INT NOT NULL, 
+    [CreatedAt] DATETIME NOT NULL, 
+    CONSTRAINT [FK_Reply_Message] FOREIGN KEY ([MessageId]) REFERENCES [Message]([Id]), 
+    CONSTRAINT [FK_Reply_User] FOREIGN KEY ([UserId]) REFERENCES [User]([Id])
+)
+
+GO
+
+CREATE INDEX [IX_Reply_MessageId_CreatedAt] ON [dbo].[Reply] ([MessageId], [CreatedAt])
+
+GO
+
+CREATE INDEX [IX_Reply_MessageId] ON [dbo].[Reply] ([MessageId])
