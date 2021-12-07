14bd4170591cab5930a5adb38126d1c35f120bd2
Mahmoud Ahmed <mhmoud.aboalazm@gmail.com>
2021-12-07 11:53:47 +0200

    Add stored procedure to get users by email, Modify the User table to add PasswordHash column for storing hashed passwords

--- Guestbook.DB/dbo/Tables/User.sql
+++ Guestbook.DB/dbo/Tables/User.sql
@@ -3,5 +3,10 @@
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
     [FirstName] NVARCHAR(50) NOT NULL, 
     [LastName] NVARCHAR(50) NOT NULL, 
-    [Email] NVARCHAR(50) NOT NULL
+    [Email] NVARCHAR(50) NOT NULL, 
+    [PasswordHash] NVARCHAR(50) NOT NULL
 )
+
+GO
+
+CREATE UNIQUE INDEX [IX_User_Email] ON [dbo].[User] ([Email])
---
+++ Guestbook.DB/dbo/storedProcedures/spUser_GetByEmail.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[spUser_GetByEmail]
+	@Email nvarchar(50)
+AS
+begin
+	select FirstName, LastName, Email
+	from dbo.[User]
+	where Email = @Email;
+end
