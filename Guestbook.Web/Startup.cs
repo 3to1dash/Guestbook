b6dae0aaa0911c210d55ef632ef9ff781ea3c575
Mahmoud Ahmed <mhmoud.aboalazm@gmail.com>
2021-12-07 12:03:43 +0200

    Add  UserData Access with dependency injection

--- Guestbook.Web/Startup.cs
+++ Guestbook.Web/Startup.cs
@@ -1,3 +1,5 @@
+using DataAccess.Data;
+using DataAccess.DbAccess;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -24,6 +26,8 @@ namespace Guestbook.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddSingleton<ISqlDataAccess, SqlDataAccess>();
+            services.AddSingleton<IUserData, UserData>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
